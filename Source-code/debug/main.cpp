2214ec4905ac0954041800a534d1c3010fb987a6
vAlerain <3506561279@qq.com>
2024-05-17 19:18:24 +0800

    添加执行CMD命令的功能.

--- Source-code/debug/main.cpp
+++ Source-code/debug/main.cpp
@@ -186,47 +186,50 @@ int main() {
     COLOR_PRINT("\\ \\ / / _ \\ | |/ _ \\ '__/ _` | | '_ \\ _____ / _ \\ | '__| |/ /\"\n", 3);
     COLOR_PRINT(" \\ V / ___ \\| |  __/ | | (_| | | | | |_____/ ___ \\| |  |   < \n", 3);
     COLOR_PRINT("  \\_/_/   \\_\\_|\\___|_|  \\__,_|_|_| |_|    /_/   \\_\\_|  |_|\\_\\ \n", 3);
-
-    COLOR_PRINT("\nvAlerain ARK menu\n [*]Enter 1 to obtain the process list\n    [*]Enter 3 to end the process\n    [*]Enter 4 to end the process tree \n [*]Enter 5 to obtain window message management\n  [*]Enter 6 to obtain the window handle where the mouse is located\n[*]Enter GetTime get system time\n[*]Enter about to obtain information about\n [*]Enter exit to exit\n",4);
+    bool cmd = false;
+    string pc= "PC vAlerain-Ark>";
+    COLOR_PRINT("\nvAlerain ARK menu\n [*]Enter 1 to obtain the process list\n    [*]Enter 3 to end the process\n    [*]Enter 4 to end the process tree \n [*]Enter 5 to obtain window message management\n  [*]Enter 6 to obtain the window handle where the mouse is located\n[*]Enter GetTime get system time\n[*]Input CMD to obtain simulated CMD terminal\n[*]Enter about to obtain information about\n [*]Enter exit to exit\n",4);
     while(true){
     string input="";
-    COLOR_PRINT("PC vAlerain-Ark>",1);
+
+    COLOR_PRINT(pc.c_str(),1);
     getline(std::cin,input);
-    if(input == "1") {
+
+    if(input == "1" && cmd == false) {
         string input_proce="";
         cout<<"Input frequency to control the speed of the acquisition process in milliseconds:";
         getline(std::cin,input_proce);
         Get_all_processes(stoi(input_proce)); //为了解决getline只能读取字符串的原因使用stoi用来更正
-    }else if(input == "about"){
+    }else if(input == "about" && cmd == false){
         COLOR_PRINT("CLion's technical support\n"
                     "vAlerain Develop;Code from Mr. vAlerain;\n"
                     "Long term evaluation and repair of SNbing54\n"
                     "Version: 1.0.0.6 (debugging)\n",1);
-        }else if(input == "exit"){
+        }else if(input == "exit" && cmd == false){
             return 0;
-        }else if(input == "3"){
+        }else if(input == "3" && cmd == false){
             DWORD processID;
             cout<<"\nEnter the process ID to end the process:";
             cin>>processID;
             TerminateProcessByID(processID);
-        }else if(input =="4"){
+        }else if(input =="4" && cmd == false){
             DWORD processID_;
             cout<<"Enter process PID to end the process:";
             cin>>processID_;
             TerminateProcessTree(processID_);
-        }else if(input == ""){
+        }else if(input == "" && cmd == false){
             COLOR_PRINT("\nWarning: Your input of empty data cannot be parsed!\n\n",6);
-        }else if(input == "memu"){
+        }else if(input == "memu" && cmd == false){
         COLOR_PRINT("\nvAlerain ARK menu\n [*]Enter 1 to obtain the process list\n    [*]Enter 3 to end the process\n    [*]Enter 4 to end the process tree \n [*]Enter about to obtain information about\n [*]Enter exit to exit\n",4);
-        }else if(input == "6"){
+        }else if(input == "6" && cmd == false){
             Sleep(3000);
             cout<<"[-]"<<hwnd_to_int(GetForegroundWindow())<<"\n";
-        }else if(input == "test-debug"){
+        }else if(input == "test-debug" && cmd == false){
             int hWnd=0;
             COLOR_PRINT("Input Test Window Handle:",3);
             cin>>hWnd;
             window_hwnd_control(1,hWnd);
-        }else if(input == "get-prx"){
+        }else if(input == "get-prx" && cmd == false){
         // 创建进程快照
         HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
         if (hSnapshot == INVALID_HANDLE_VALUE) {
@@ -270,20 +273,24 @@ int main() {
         // 关闭进程快照句柄
         CloseHandle(hSnapshot);
 
-        }else if(input == "5"){
+        }else if(input == "5" && cmd == false){
             COLOR_PRINT("Kill Window:",4);
             string hwnd_temp="";
             getline(std::cin,hwnd_temp);
             window_hwnd_control(1,stoi(hwnd_temp));
-        }else if(input =="GetTime"){
+        }else if(input =="GetTime" && cmd == false){
             SYSTEMTIME temp;
             GetLocalTime(&temp);
             printf("%04d/%02d/%02d %02d:%02d:%02d\n", temp.wYear, temp.wMonth, temp.wDay, temp.wHour, temp.wMinute, temp.wSecond);
 
+        }else if(input == "CMD" && cmd == false){
+            pc="PC vAlerain-Ark(CMD)>";
+            cmd = true;
+        }else if(cmd == true){
+            system(input.c_str());
         }else{
-            COLOR_PRINT("\nError: You entered an incorrect parameter that cannot be parsed into any data!\n\n",4);
+            if(cmd == false){COLOR_PRINT("\nError: You entered an incorrect parameter that cannot be parsed into any data!\n\n",4);}
         }
 
     }
-    return 0;
 }
